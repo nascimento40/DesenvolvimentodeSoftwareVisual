5617b9437749c728a08cd8e2740a5c445133ec52
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-08-10 22:31:08 -0300

    Add files via upload

---
+++ Aula10-08/Anotacoes.cs
@@ -0,0 +1,4 @@
+//dotnet new console
+//dotnet new webapi
+// --name (escolhe o nome)
+// dotnet run / debug no Program.cs (programa criado pelo dotnet new console)
---
+++ Aula10-08/hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static double calculaArea (double largura, double altura) {
+            return largura * altura;
+        }
+        static void Main(string[] args)
+        {
+            try {
+            Console.WriteLine("Digite uma largura:");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite uma altura:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("A area é: ");
+            double area = calculaArea(largura, altura);
+            Console.WriteLine(area);
+            }
+            catch(Exception execao) {
+                Console.WriteLine(execao.Message);
+                //Console.WriteLine("Numero invalido");
+            }
+        }
+    }
+}
