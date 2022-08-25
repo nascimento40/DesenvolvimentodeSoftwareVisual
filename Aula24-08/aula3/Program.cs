f264e635af6fd2afff4ab6fff66f5b0c35b53eb4
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-08-24 22:20:21 -0300

    Add files via upload

---
+++ Aula24-08/aula3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Bem vindo ao sistema de login");
+            Console.WriteLine("Digite 1 para logar");
+            Console.WriteLine("Digite 2 para cadastrar");
+            Console.WriteLine("Digite 3 para sair");
+            bool sair = false;
+            while(!sair){
+                int opcao = 0;
+                try{
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch(Exception e){
+                    //Console.WriteLine("valor invalido, tente novamente");
+                }
+                switch(opcao){
+                    case 1:
+                        //todo logar
+                    break;
+                    case 2:
+                        //todo cadastrar
+                    break;
+                    case 3:
+                        sair = true;
+                    break;
+                    default:
+                        Console.WriteLine("opcao invalida! tente novamente");
+                    break;
+                }
+            }
+        }
+    }
+}
