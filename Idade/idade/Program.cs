b93878dd577ec83eedf57616fa5843442008f28d
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-08-31 17:43:38 -0300

    Add files via upload

---
+++ Idade/idade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace idade
+{
+	class Program
+	{
+		
+		public static void Main(string[] args)
+		{
+            Console.WriteLine("Digite a idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            
+            if (idade <= 13) {
+            Console.WriteLine("Criança");
+            } else if (idade > 13 && idade <= 18) {
+            Console.WriteLine("Adolescente");
+            } else if (idade > 18 && idade <= 60) {
+            Console.WriteLine("Adulto");
+            } else if (idade > 60) {
+            Console.WriteLine("Idoso");
+            }
+		}
+	}
+}
