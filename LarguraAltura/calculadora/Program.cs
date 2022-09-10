6f725f408088a1ab2963abefe95c208ad187043d
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-09-10 14:09:04 -0300

    Add files via upload

---
+++ LarguraAltura/calculadora/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace calculadora
+{
+	class Program
+	{
+		static double CalculaArea(double largura, double altura)
+		{
+			return largura * altura;
+		}
+		
+		public static void Main(string[] args)
+		{
+			double largura = 0.0;
+			double altura = 0.0;
+			Console.WriteLine("Bem vindo a calculadora de area! :v");
+			while(true)
+			{
+				try
+				{
+					Console.WriteLine("Insira uma largura:");
+					largura = double.Parse(Console.ReadLine());
+					break;
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+			while(true)
+			{
+				try
+				{
+					Console.WriteLine("Agora insira uma altura:");
+					altura = double.Parse(Console.ReadLine());
+					break;
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+			Console.WriteLine("A area eh " + CalculaArea(largura, altura));
+			Console.Write("Pressione 'enter' para sair");
+			Console.ReadLine();
+		}
+	}
+}
