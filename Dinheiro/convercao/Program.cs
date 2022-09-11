b6bd299a4a6bbc35a43553731b45d63193b2b186
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-09-10 22:36:36 -0300

    Add files via upload

---
+++ Dinheiro/convercao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProgVisual
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Conversor de moedas");
+			double valor = 0.0;
+			while(true)
+			{
+				try
+				{
+					Console.WriteLine("Insira um valor em reais:");
+					valor = double.Parse(Console.ReadLine());
+					break;
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+			Console.WriteLine("Valor em reais: "   + valor);
+			Console.WriteLine("Valor em dolares: " + (valor / 5.17));
+			Console.WriteLine("Valor em euros: "   + (valor / 6.14));
+			Console.WriteLine("Valor em pesos: "   + (valor / 0.05));
+			Console.Write("Pressione 'enter' para sair");
+			Console.ReadLine();
+		}
+	}
+}
