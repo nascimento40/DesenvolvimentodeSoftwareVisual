beac6c5f48aa6260dc429ae710ff17ac2a8ef4fc
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-09-21 20:44:03 -0300

    Add files via upload

---
+++ teste2/teste2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace teste2
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Digite o primeiro numero: ");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    int y = int.Parse(Console.ReadLine());
+
+    if (x > y)
+    {
+        Console.WriteLine(x + " é maior do que " + y);
+    }
+    else
+    {
+        Console.WriteLine(y + " é maior do que " + x);
+    }
+    //https://sites.google.com/site/inflp2009/2--instrucoes-compostas/if-else/maior-de-dois-numeros        
+    }
+    }
+}
