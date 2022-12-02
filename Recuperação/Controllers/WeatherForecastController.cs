8adfbbf0d80486463be94463835d9ab776449b68
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-12-01 22:20:06 -0300

    Add files via upload

---
+++ Recuperação/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Recuperação.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public Pessoa Post(Pessoa usuario) 
+        {
+            usuario.nasc.AddYears(1);
+            return usuario;
+        }
+    }
+}
---
+++ Recuperação/WeatherForecast.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Recuperação
+{
+    public class Pessoa {
+        public string nome {get; set;}
+        public DateTime nasc {get; set;}
+    }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
